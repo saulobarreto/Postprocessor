741679add6bc0be4549f3d5fb819c2949ca91338
Saulo Vinicius Barreto <47115363+saulobarreto@users.noreply.github.com>
2022-02-17 09:27:20 -0300

    First commit.

---
+++ postprocessing.m
@@ -0,0 +1,48 @@
+function postprocessing( sample, treshold = 0.8)
+  
+  close all;
+  
+  num_elements = 200;
+  xaxis= 1:num_elements;
+  
+  input = rand(1,num_elements);
+  treshold_vector = ones(1,num_elements).*treshold;
+  ouput = zeros(1,num_elements);
+  
+  DEBOUNCE_COUNTER = 5;
+  activation_result = false;
+  
+  for ( counter = 1 : num_elements )
+    
+    if( (input(counter) >= treshold) && ( DEBOUNCE_COUNTER == 5 ) )
+    
+      activation_result = true;
+      
+    endif
+    
+    if ( activation_result == true)
+      
+      DEBOUNCE_COUNTER = DEBOUNCE_COUNTER - 1;
+      
+    endif
+    
+    if( (input(counter) < treshold) && ( DEBOUNCE_COUNTER == -1 ) )
+      
+      activation_result = false;
+      DEBOUNCE_COUNTER = 5;
+      
+    endif
+    
+    output(1,counter) = activation_result;
+    
+  endfor
+  
+  figure();
+  hold on;
+  plot(xaxis,input,"marker",".");
+  plot(xaxis,treshold_vector);
+  plot(xaxis,output);
+  grid on;
+  legend("Inference score","Treshold","Activation");
+  
+endfunction
